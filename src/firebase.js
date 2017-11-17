528e98376c56be96d2b44cc73d2e5335773faa2d
Kris Zima <kris@mosquito.ie>
2017-11-17 16:52:23 +1000

    add global env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,14 @@
 import * as firebase from "firebase";
 // import "firebase/firestore";
 
+console.log("project.env", process.env);
+
 // should go in a secret file
 firebase.initializeApp({
-  apiKey: process.env.REACT_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_FIREBASE_DB,
-  projectId: process.env.REACT_PROJECT_ID
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB,
+  projectId: process.env.REACT_APP_PROJECT_ID
 });
 
 export const firestore = firebase.firestore();
